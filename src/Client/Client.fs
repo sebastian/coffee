425c25a52f7acd7d11a04885558bedefbfc1277c
Sebastian Probst Eide <sebastian@aircloak.com>
2019-05-26 20:51:02 +0200

    Add explanation

--- src/Client/Client.fs
+++ src/Client/Client.fs
@@ -92,6 +92,13 @@ let update (msg : Msg) (currentModel : Model) : Model * Cmd<Msg> =
 
 let renderWelcome dispatch =
     div [ClassName "start"] [
+        h1 [] [str "What is this?"]
+        p [] [str (
+            "This is a coffee timer for the \"4:6 method\" invented by Tetsu Kasuya. " +
+            "It times 5 pours of 45 seconds each."
+        )]
+        a [Href "https://www.youtube.com/watch?v=wmCW8xSWGZY"] [str "This video explains the process"]
+
         h1 [] [str "Start brewing?"]
         p [] [str "How much coffee are you using?"]
         div [ClassName "amount-coffee"] [
